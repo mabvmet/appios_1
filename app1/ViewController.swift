1a2361bf8e89bb9886598346fadb0ad1a1ae6051
mabvmet <mabvmet@mabvmet-mbp.lan>
2016-11-12 00:10:14 -0600

    Initial Commit

---
+++ app1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  app1
+//
+//  Created by mabvmet on 11/12/16.
+//  Copyright © 2016 mabvmet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
